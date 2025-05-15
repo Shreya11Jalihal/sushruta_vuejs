4ebc06f0484a60d9374219de71475b23d5415e08
unknown <jalihal.shreya@gmail.com>
2025-05-15 15:52:28 +0200

    add project Sushruta to github

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div>
+  </div>
+    
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
